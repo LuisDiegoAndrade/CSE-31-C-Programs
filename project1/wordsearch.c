7dc7c52b4820e8345cd9f7408116f5685a4c3c3d
Luis Andrade <luisdiego26.la@gmail.com>
2020-03-05 01:58:17 -0800

    finished project1 ;)

--- project1/wordsearch.c
+++ project1/wordsearch.c
@@ -1,3 +1,16 @@
+
+/*
+██▓     █    ██  ██▓  ██████
+▓██▒     ██  ▓██▒▓██▒▒██    ▒
+▒██░    ▓██  ▒██░▒██▒░ ▓██▄
+▒██░    ▓▓█  ░██░░██░  ▒   ██▒
+░██████▒▒▒█████▓ ░██░▒██████▒▒
+░ ▒░▓  ░░▒▓▒ ▒ ▒ ░▓  ▒ ▒▓▒ ▒ ░
+░ ░ ▒  ░░░▒░ ░ ░  ▒ ░░ ░▒  ░ ░
+ ░ ░    ░░░ ░ ░  ▒ ░░  ░  ░
+   ░  ░   ░      ░        ░
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +20,9 @@
 // Feel free to declare any helper functions
 void printPuzzle(char** arr, int n);
 /*char** arr, int n, */
-void searchPuzzle(char** list, int listSize);
+void searchPuzzle(char** block, int blocksize, char** word, int listsize);
 
-void toUpperCase(char *word);
+void toLowerCase(char *word);
 
 char* left2right(char** puzzle, int row, int start, int characters);
 
@@ -21,8 +34,13 @@ char* downleft(char** puzzle, int row, int start, int characters);
 
 char* downright(char** puzzle, int row, int start, int characters);
 
+int areEqual(char *word, char* found, int size);
 
-
+void l2rLower(char**puzzle, int row, int start, int size);
+void r2lLower(char**puzzle, int row, int start, int size);
+void downLower(char**puzzle, int row, int start, int size);
+void dlLower(char**puzzle, int row, int start, int size);
+void drLower(char**puzzle, int row, int start, int size);
 
 
 // Main function, DO NOT MODIFY!!!
@@ -72,7 +90,7 @@ int main(int argc, char **argv) {
 
 	// Print out word list
 	printf("Printing list of words:\n");
-	for(i=0; i<50; i++){
+	for(i=0; i<50; i++) {
 		printf("%s\n", *(words + i));
 	}
 	printf("\n");
@@ -81,18 +99,17 @@ int main(int argc, char **argv) {
     printf("Printing puzzle before search:\n");
     printPuzzle(block, bSize);
 	printf("\n");
-  searchPuzzle(words,50);
+
+
+  searchPuzzle(block, bSize, words, 50);
+
+  /*for (k = 0; k < 50; ++k) {
+      searchPuzzle(block, 15, *(words+k));
+  } */
+
   printf("\n");
-  printf("%s\n", *(words));
-
-  toUpperCase(*(words));
-  printf("%s\n", *(words));
-
-  char* lr = downright(block, 0, 14, 3);
-  printf("%s\n", lr);
-
-
-
+  printf("Printing puzzle AFTER search:\n");
+  printPuzzle(block, bSize);
 
 	// Call searchPuzzle to find all words in the puzzle
 	//searchPuzzle(block, bSize, words, 50);
@@ -121,17 +138,79 @@ void printPuzzle(char** arr, int n){
 
 }
 /*char** arr,int n,*/
-void searchPuzzle(char** list, int listSize){
-  int i, j;
-  char ** search_matrix;
+void searchPuzzle(char** block, int blocksize, char** word, int listsize){
+  int i, j, k, size;
+  char *lr, *rl, *down, *dl, *dr;
+  char firstletter;
+  char tmp;
+
+  for (k = 0; k < listsize; ++k) {
+    toLowerCase(*(word+k));
+
+    size = strlen(*(word+k));
+    firstletter = *(*(word+k));
+
+    for (i = 0; i < 15; ++i) {
+      for (j = 0; j < 15; ++j) {
+
+        //some more ravioli here
+        if ( (int) *(*(block+i)+j) > 64 && (int) *(*(block+i)+j) < 91 ) {
+          tmp = (char)((int) *(*(block+i)+j) +32);
+        } else {
+          tmp = *(*(block+i)+j);
+        }
+        //
+
+        if ( tmp == firstletter ) {
+          lr = left2right(block, i, j, size);
+          //printf("%s\n", lr);
+          rl = right2left(block, i, j, size);
+          //printf("%s\n", rl);
+          down = downwards(block, i, j, size);
+          //printf("%s\n", down);
+          dl = downleft(block, i, j, size);
+          //printf("%s\n", dl);
+          dr = downright(block, i, j, size);
+          //printf("%s\n", dr);
+
+          if (areEqual(*(word+k), down, size)) {
+            printf("Found %s!\n", down);
+            downLower(block, i, j, size);
+          }
+
+          if (areEqual(*(word+k), lr, size)) {
+            printf("Found %s!\n", lr);
+            l2rLower(block, i, j, size);
+
+          }
+
+          if (areEqual(*(word+k), dl, size)) {
+            printf("Found %s!\n", dl);
+            dlLower(block, i, j, size);
+          }
+          if (areEqual(*(word+k), dr, size)) {
+            printf("Found %s!\n", dr);
+            drLower(block, i, j, size);
+          }
+
+          if (areEqual(*(word+k), rl, size)) {
+            printf("Found %s!\n", rl);
+            r2lLower(block, i, j, size);
+          }
+
+        }
+      }
+    } // for loop end
+
+  }
 
 }
 
-void toUpperCase(char *word) {
+void toLowerCase(char *word) {
   char* p;
   for( p=word; *p!=0; p++ ) {
-    if ((int)*p > 96 && (int)*p < 123) {
-      *(p) = (char)((int)*p - 32);
+    if ((int)*p > 64 && (int)*p < 91) {
+      *(p) = (char)((int)*p + 32);
     }
   }
 
@@ -144,15 +223,13 @@ char* left2right(char** puzzle, int row, int start, int characters) {
 
   arr = (char*)malloc(characters*sizeof(char));
 
-  /*for (p = *(*(puzzle+row)+start); p !=0; p++) {
 
-  }*/
   for (i = start; i < 15; ++i){
     if(c == characters) {
       break;
     } else {
 
-      *(arr+i) = *(*(puzzle+row)+i);
+      *(arr+i-start) = *(*(puzzle+row)+i);
     }
     c++;
 
@@ -174,7 +251,7 @@ char* right2left(char** puzzle, int row, int start, int characters) {
     if(c == characters) {
       break;
     } else {
-      *(arr+i) = *(*(puzzle+row)+i);
+      *(arr+i-start) = *(*(puzzle+row)+i);
     }
     c++;
 
@@ -193,7 +270,7 @@ char* downwards(char** puzzle, int row, int start, int characters) {
     if (c == characters) {
       break;
     } else {
-      *(arr+i) = *(*(puzzle+i)+start);
+      *(arr+i-row) = *(*(puzzle+i)+start);
     }
     c++;
   }
@@ -213,7 +290,7 @@ char* downleft(char** puzzle, int row, int start, int characters) {
     if (c == characters || start == 0) {
       break;
     } else {
-      *(arr+i) = *(*(puzzle+i)+start);
+      *(arr+i-row) = *(*(puzzle+i)+start);
     }
     c++;
     start--;
@@ -233,11 +310,126 @@ char* downright(char** puzzle, int row, int start, int characters) {
     if (c == characters || start == 14) {
       break;
     } else {
-      *(arr+i) = *(*(puzzle+i)+start);
+      *(arr+i-row) = *(*(puzzle+i)+start);
     }
     c++;
     start++;
   }
 
   return arr;
+}
+
+int areEqual(char* word, char* found, int size) {
+  int i;
+  for(i = 0; i < size; ++i) {
+
+    if(*(found+i) > 64 && *(found+i) < 91) {
+      *(found+i) = (char)((int) *(found+i) + 32);
+    } else {
+      *(found+i) = *(found+i);
+    }
+
+    if(*(word+i) != *(found+i)) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void l2rLower(char **puzzle, int row, int start, int size){
+  int c = 0;
+  int i;
+
+  for (i = start; i < 15; ++i) {
+    if (c == size ) {
+      break;
+    } else {
+      if ((int)*(*(puzzle+row)+i) > 64 && (int)*(*(puzzle+row)+i) < 91) {
+          *(*(puzzle+row)+i) = (char)((int)*(*(puzzle+row)+i)+32);
+      } else {
+          *(*(puzzle+row)+i) = *(*(puzzle+row)+i);
+      }
+    }
+    c++;
+  }
+}
+
+void r2lLower(char **puzzle, int row, int start, int size){
+  int c = 0;
+  int i;
+
+  for (i = start; i >= 0; --i) {
+    if (c == size) {
+      break;
+    } else {
+      if ((int) *(*(puzzle+row)+i) > 64 && (int) *(*(puzzle+row)+i) < 91) {
+          *(*(puzzle+row)+i) = (char)((int)*(*(puzzle+row)+i)+32);
+      } else {
+          *(*(puzzle+row)+i) = *(*(puzzle+row)+i);
+      }
+
+    }
+    c++;
+  }
+}
+
+void downLower(char **puzzle, int row, int start, int size){
+  int c = 0;
+  int i;
+
+  for (i = row; i < 15; ++i) {
+    if (c == size) {
+      break;
+    } else {
+      if ((int) *(*(puzzle+i)+start) > 64 && (int) *(*(puzzle+i)+start) < 91) {
+        *(*(puzzle+i)+start) = (char)((int)*(*(puzzle+i)+start)+32);
+      } else {
+        *(*(puzzle+i)+start) = *(*(puzzle+i)+start);
+      }
+
+    }
+    c++;
+  }
+
+}
+
+void dlLower(char **puzzle, int row, int start, int size){
+  int c = 0;
+  int i;
+
+  for (i = row; i < 15; ++i) {
+    if (c == size || start == 0) {
+      break;
+    } else {
+      if ((int) *(*(puzzle+i)+start) > 64 && (int) *(*(puzzle+i)+start) < 91) {
+        *(*(puzzle+i)+start) = (char)((int)*(*(puzzle+i)+start)+32);
+      } else {
+        *(*(puzzle+i)+start) = *(*(puzzle+i)+start);
+      }
+
+    }
+    c++;
+    start--;
+  }
+
+}
+
+void drLower(char **puzzle, int row, int start, int size){
+  int c = 0;
+  int i;
+
+  for (i = row; i < 15; ++i) {
+    if (c == size || start == 14) {
+      break;
+    } else {
+      if ((int) *(*(puzzle+i)+start) > 64 && (int) *(*(puzzle+i)+start) < 91) {
+        *(*(puzzle+i)+start) = (char)((int)*(*(puzzle+i)+start)+32);
+      } else {
+        *(*(puzzle+i)+start) = *(*(puzzle+i)+start);
+      }
+
+    }
+    c++;
+    start++;
+  }
 }
